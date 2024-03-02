aa71d6b652f4da93413114f881cfe945a7160596
Jeewon Kim <158134605+one1won57@users.noreply.github.com>
2024-03-02 21:56:28 +0900

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -1,12 +1,14 @@
 import torch
 import numpy as np
-import matplotlib.pyplot as plt
 
 def real_imag(kspace, coilN):
     kspace_real = kspace[:,:coilN,:,:]
     kspace_imag = kspace[:,coilN:,:,:]
     res = kspace_real +1j*kspace_imag
     return res 
+
+def RSSq(img):
+    return (torch.sum(abs(img)**2,1)**(0.5)).unsqueeze(0)
 
 def mfft1(real_imag_kspace, dim = 2):
     res = torch.fft.ifftshift(real_imag_kspace,dim=dim)
@@ -19,10 +21,18 @@ def mfft2(real_imag_kspace,dimx =2,dimy=3 ):
     fft2_kspace = mfft1(fft1_kspace,dim=dimy)
     return fft2_kspace
 
-def save_rssq(img, perc, save_img_path, name = 'rssq', cmap = 'gray', show = False):
-    img = np.fliplr(np.flipud(img.transpose(1,0)))
-    img = img/np.percentile(img,perc)
-    plt.imsave(save_img_path + '/'+name +'.jpg', img, vmin =0,vmax = 1, cmap = cmap)
-    if show == True:
-        plt.imshow(img, cmap = cmap, vmin=0,vmax=1)
-    return None
+def RSSq_np(img):
+    return np.expand_dims((np.sum(np.abs(img)**2,1)**(0.5)),axis=0)
+
+
+def mfft1_np(real_imag_kspace, axis = 2):
+    res = np.fft.ifftshift(real_imag_kspace,axes=axis)
+    res = np.fft.ifft(res,axis=axis)
+    res = np.fft.ifftshift(res,axes=axis)
+    return res
+
+def mfft2_np(real_imag_kspace,dimx =2,dimy=3 ):
+    fft1_kspace = mfft1_np(real_imag_kspace,axis=dimx)
+    fft2_kspace = mfft1_np(fft1_kspace,axis=dimy)
+    return fft2_kspace
+
